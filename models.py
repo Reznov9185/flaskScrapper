0563e8f6ab1f44b7bfc67190ee798b927145fc86
Reznov9185 <reznov9185@gmail.com>
2020-04-18 23:05:47 +0600

    Tested Some Youtube v3 APIs & DB mysql connection added - youtube stat + channel videos api tested - added mysql connection with SQLAlchemy with mysql driver

---
+++ models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+from sqlalchemy.inspection import inspect
+
+class Serializer(object):
+
+    def serialize(self):
+        return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
+
+    @staticmethod
+    def serialize_list(l):
+        return [m.serialize() for m in l]
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    email = Column(String(120), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+    def serialize(self):
+        d = Serializer.serialize(self)
+        return d
